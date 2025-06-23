1b52fcfc8f6e56acd6f4fd66b056cf5d5d604e7e
Mukunthan Mahesh <mukunthanmahesh@gmail.com>
2025-06-23 16:30:45 -0400

    chore: SQW-34 - installed framer-motion

---
+++ src/components/LandingPage/AboutSection.jsx
@@ -0,0 +1,15 @@
+
+const AboutSection = () => {
+  return (
+    <section className="max-w-7xl mx-auto px-4 py-12">
+      <div className="mb-10">
+        <h2 className="font-crimson text-4xl font-bold mt-2 mb-4 ">Who Are We?</h2>
+        <p className="font-crimson text-lg text-gray-800">
+          SciNapse Queen's works towards <strong>The Undergraduate Science Case Competition (USCC)</strong>, which is an Ontario-wide competition where groups of 2-4 attempt to create an interdisciplinary novel research proposal for this year's topic. The competition consists of two rounds. In the first round, research proposals will be judged by local professors and PhD students. The top 10% of teams then head to the provincial round in March in Ottawa to compete against universities from across Ontario by presenting their posters at the USCC. Our club is responsible for facilitating the conference for Queen's students, as well as hosting events throughout the year to promote undergraduate research and innovation.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroBanner from "../components/LandingPage/HeroBanner";
 import EventSection from "../components/LandingPage/EventSection";
+import AboutSection from "../components/LandingPage/AboutSection";
 
 export default function Clinics() {
   return (
     <>
     <HeroBanner />
     <EventSection />
+    <AboutSection />
     </>
   );
   }
