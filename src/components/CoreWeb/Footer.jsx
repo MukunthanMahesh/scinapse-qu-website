1e678a8279df364169874d3ae945ac2275890962
Mukunthan Mahesh <mukunthanmahesh@gmail.com>
2025-06-27 00:22:05 -0400

    feat: add footer logo for mobile

--- src/components/CoreWeb/Footer.jsx
+++ src/components/CoreWeb/Footer.jsx
@@ -4,11 +4,17 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-[1fr_2fr_1.5fr] gap-6">
         
         {/* COLUMN 1: Footer About Section */}
-        <div>
+        <div className="">
+
+          {/* Logo Above (Phone) */}
+          <div className="flex sm:hidden justify-center mb-8">
+            <img src="/assets/logo.png" alt="SciNapse Logo" className="h-auto w-auto" />
+          </div>
+
           {/* About Text Square */}
           <div className="p-1.5 bg-gradient-to-r from-purple-500 to-brand-cyanBlue mb-4">
             <div className="bg-brand-black text-brand-black p-4 rounded-md flex flex-col justify-center">
-              <div className="bg-black text-white p-4 rounded-md self-start sm:self-stretch sm:-mr-[25%] -mr-[10%]">
+              <div className="bg-black text-white p-4 rounded-md sm:min-h-0 self-start sm:self-stretch sm:-mr-[25%] -mr-[10%] content-center">
                 <p class="leading-snug text-lg mb-2"> Our mission is to empower Queen's Students to communicate science beyond the lecture halls. </p>
                 <p class="leading-snug text-sm mb-1">We offer hands-on research experience to undergraduates every year through cross-club events, the USCC, and STEM Horizons. </p>
                 <p class="leading-snug text-sm">Our chapter, based at <strong>Queen’s University in Kingston </strong>, works with national networks to expand science communication across Canada.</p>
