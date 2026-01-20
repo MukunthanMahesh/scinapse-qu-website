64c38ae5ddfaa5c8f3dcae4b6fe8586518ef17c4
Mukunthan Mahesh <mukunthanmahesh@gmail.com>
2026-01-20 00:10:18 -0500

    feat: team page overhaul

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -2,79 +2,106 @@ export const teamMembers = [
   {
     name: "Tulassi Vije",
     role: "Co-President",
-    image: "/team/tulassivije.jpg",
-    linkedin: "https://www.linkedin.com/in/tulassi-vije/"
+    image: "/team/2025-2026/tulassivije.jpg",
+    yearProgram: "3rd Year, Health Sciences",
+    funFact: "\"I love iced brown sugar lattes with an extra shot of espresso.\""
   },
   {
     name: "Soha Kar",
     role: "Co-President",
-    image: "/team/sohakar.jpg",
-    linkedin: "https://www.linkedin.com/in/soha-kar/"
+    image: "/team/2025-2026/sohakar.jpg",
+    yearProgram: "3rd Year, Health Sciences",
+    funFact: "Has an ever-growing collection of astronomy-themed mugs."
   },
   {
-    name: "Hiring in Progress",
-    role: "Conference Coordinator",
-    image: "/team/placeholder-hiring-f.svg",
-    linkedin: null
+    name: "Rami Idris",
+    role: "Conference & Logistics Coordinator",
+    image: "/team/2025-2026/ramiidris.png",
+    yearProgram: "3rd Year, Health Sciences",
+    funFact: "\"my most listened to artist is Brent Faiyaz.\""
+  },
+  {
+    name: "Liam Lahiffe",
+    role: "Conference & Logistics Coordinator",
+    image: "/team/2025-2026/liamlahiffe.jpg",
+    yearProgram: "3rd Year, Biochemistry",
+    funFact: "Did not submit the google form ☹️"
+  },
+  {
+    name: "Allen Tian",
+    role: "Treasurer",
+    image: "/team/2025-2026/allentian.png",
+    yearProgram: "PhD (Biology, very old)",
+    funFact: "\"I can play any woodwind instrument.\""
+  },
+  {
+    name: "Emmeline He",
+    role: "Finance Intern",
+    image: "/team/2025-2026/emmelinehe.jpg",
+    yearProgram: "3rd Year, Life Sciences",
+    funFact: "\I have a fraternal twin sister!\""
+  },
+  {
+    name: "Ravetta Ravichandran",
+    role: "Social Media Manager",
+    image: "/team/2025-2026/ravettaravichandran.png",
+    yearProgram: "3rd Year, Health Sciences",
+    funFact: "\"I have a twin brother!\""
+  },
+  {
+    name: "Shreyas Konda",
+    role: "Outreach Coordinator",
+    image: "/team/2025-2026/shreyaskonda.jpg",
+    yearProgram: "3rd Year, Health Sciences",
+    funFact: "\"I like Osmows a little too much!\""
+  },
+  {
+    name: "Ali Mezher",
+    role: "Outreach Coordinator",
+    image: "/team/2025-2026/alimezher.webp",
+    yearProgram: "Year & program coming soon",
+    funFact: "\"I love skiing.\""
+  },
+  {
+    name: "Ella Gronlund",
+    role: "Marketing Coordinator",
+    image: "/team/2025-2026/ellagronlund.jpeg",
+    yearProgram: "3rd Year, Life Sciences",
+    funFact: "\"I can do a headstand!\""
+  },
+  {
+    name: "Riya Chopra",
+    role: "Marketing Intern",
+    image: "/team/2025-2026/riyachopra.jpg",
+    yearProgram: "2nd Year, Health Sciences",
+    funFact: "\"I've watched the movie Interstellar 18 times!\""
+  },
+  {
+    name: "Amy Song",
+    role: "Logistics Intern",
+    image: "/team/2025-2026/amysong.webp",
+    yearProgram: "1st Year, Health Sciences",
+    funFact: "\"I love rock climbing or rollerblading by the pier to stay active!\""
+  },
+  {
+    name: "Haneen Osman",
+    role: "First-Year Representative",
+    image: "/team/2025-2026/haneenosman.png",
+    yearProgram: "1st Year, Health Sciences",
+    funFact: "\"I love to travel!!\""
+  },
+  {
+    name: "Elena Rastani",
+    role: "First-Year Representative",
+    image: "/team/2025-2026/elenarastani.jpeg",
+    yearProgram: "1st Year, Life Sciences & Biochemistry",
+    funFact: "\"I love Star Wars!\""
   },
   {
     name: "Mukunthan Mahesh",
-    role: "Web Developer",
-    image: "/team/mukunthanmahesh.jpg",
-    linkedin: "https://www.linkedin.com/in/mukunthan-mahesh/"
-  },
-  {
-    name: "Hiring in Progress",
-    role: "Finance Director",
-    image: "/team/placeholder-hiring-f.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "Secretary",
-    image: "/team/placeholder-hiring-m.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "Outreach Coordinator",
-    image: "/team/placeholder-hiring-f.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "Outreach Coordinator",
-    image: "/team/placeholder-hiring-m.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "Marketing Coordinator",
-    image: "/team/placeholder-hiring-f.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "Logistics Coordinator",
-    image: "/team/placeholder-hiring-m.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "Logistics Coordinator",
-    image: "/team/placeholder-hiring-f.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "First-Year Representative",
-    image: "/team/placeholder-hiring-m.svg",
-    linkedin: null
-  },
-  {
-    name: "Hiring in Progress",
-    role: "First-Year Representative",
-    image: "/team/placeholder-hiring-f.svg",
-    linkedin: null
+    role: "Web Designer & Developer",
+    image: "/team/2025-2026/mukunthanmahesh.png",
+    yearProgram: "3rd Year, Computing (SODE)",
+    funFact: "Like what you see here? Let's talk! mukunthan.m@queensu.ca"
   },
 ]
