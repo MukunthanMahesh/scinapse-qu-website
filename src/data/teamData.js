d6c27dfeac205a379efff5b1099d6cb201e3412a
Mukunthan Mahesh <mukunthanmahesh@gmail.com>
2026-01-21 22:53:29 -0500

    feat: update team data

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -4,104 +4,104 @@ export const teamMembers = [
     role: "Co-President",
     image: "/team/2025-2026/tulassivije.webp",
     yearProgram: "3rd Year, Health Sciences",
-    funFact: "\"I love iced brown sugar lattes with an extra shot of espresso.\""
+    funFact: "Loves iced brown sugar lattes with a shot of espresso"
   },
   {
     name: "Soha Kar",
     role: "Co-President",
     image: "/team/2025-2026/sohakar.webp",
     yearProgram: "3rd Year, Health Sciences",
-    funFact: "Wants a farm with hella dogs."
+    funFact: "Wants to live in a farm with \"hella\" dogs"
   },
   {
     name: "Rami Idris",
     role: "Conference & Logistics Coordinator",
     image: "/team/2025-2026/ramiidris.webp",
     yearProgram: "3rd Year, Health Sciences",
-    funFact: "\"my most listened to artist is Brent Faiyaz.\""
+    funFact: "Most listened to artist is Brent Faiyaz"
   },
   {
     name: "Liam Lahiffe",
     role: "Conference & Logistics Coordinator",
     image: "/team/2025-2026/liamlahiffe.webp",
     yearProgram: "3rd Year, Biochemistry",
-    funFact: "Did not submit the google form ☹️"
+    funFact: "Did not submit the google form \:("
   },
   {
     name: "Amy Song",
     role: "Logistics Intern",
     image: "/team/2025-2026/amysong.webp",
     yearProgram: "1st Year, Health Sciences",
-    funFact: "\"I love rock climbing or rollerblading by the pier to stay active!\""
+    funFact: "Loves rock climbing and roller blading by the pier to stay active"
   },
   {
     name: "Allen Tian",
     role: "Treasurer",
     image: "/team/2025-2026/allentian.webp",
     yearProgram: "PhD (Biology, very old)",
-    funFact: "\"I can play any woodwind instrument.\""
+    funFact: "Can play any woodwind instrument"
   },
   {
     name: "Emmeline He",
     role: "Finance Intern",
     image: "/team/2025-2026/emmelinehe.webp",
     yearProgram: "3rd Year, Life Sciences",
-    funFact: "\I have a fraternal twin sister!\""
+    funFact: "Has a fraternal twin sister"
   },
   {
     name: "Ravetta Ravichandran",
     role: "Social Media Manager",
     image: "/team/2025-2026/ravettaravichandran.webp",
     yearProgram: "3rd Year, Health Sciences",
-    funFact: "\"I have a twin brother!\""
+    funFact: "Has a twin brother"
   },
   {
     name: "Ella Gronlund",
     role: "Marketing Coordinator",
     image: "/team/2025-2026/ellagronlund.webp",
     yearProgram: "3rd Year, Life Sciences",
-    funFact: "\"I can do a headstand!\""
+    funFact: "Can do a headstand"
   },
   {
     name: "Riya Chopra",
     role: "Marketing Intern",
     image: "/team/2025-2026/riyachopra.webp",
     yearProgram: "2nd Year, Health Sciences",
-    funFact: "\"I've watched the movie Interstellar 18 times!\""
+    funFact: "Watched Interstellar 18 times"
   },
   {
     name: "Shreyas Konda",
     role: "Outreach Coordinator",
     image: "/team/2025-2026/shreyaskonda.webp",
     yearProgram: "3rd Year, Health Sciences",
-    funFact: "\"I like Osmows a little too much!\""
+    funFact: "Likes Osmows a lil too much"
   },
   {
     name: "Ali Mezher",
     role: "Outreach Coordinator",
     image: "/team/2025-2026/alimezher.webp",
-    yearProgram: "Year & program coming soon",
-    funFact: "\"I love skiing.\""
+    yearProgram: "1st Year, Health Sciences",
+    funFact: "Loves to skii"
   },
   {
     name: "Haneen Osman",
     role: "First-Year Representative",
     image: "/team/2025-2026/haneenosman.webp",
     yearProgram: "1st Year, Health Sciences",
-    funFact: "\"I love to travel!!\""
+    funFact: "Loves to travel"
   },
   {
     name: "Elena Rastani",
     role: "First-Year Representative",
     image: "/team/2025-2026/elenarastani.webp",
     yearProgram: "1st Year, Life Sciences & Biochemistry",
-    funFact: "\"I love Star Wars!\""
+    funFact: "Loves Star Wars "
   },
   {
     name: "Mukunthan Mahesh",
     role: "Web Designer & Developer",
     image: "/team/2025-2026/mukunthanmahesh.webp",
     yearProgram: "3rd Year, Computing (SODE)",
-    funFact: "Like what you see here? Let's talk! mukunthan.m@queensu.ca"
+    funFact: "I love my airfryer"
   },
 ]
