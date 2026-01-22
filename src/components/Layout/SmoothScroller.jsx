7dfd404cca31608f16e56e54f6ccb5ffac6fff0b
Mukunthan Mahesh <mukunthanmahesh@gmail.com>
2026-01-22 00:20:01 -0500

    fix: increase scroll speed

--- src/components/Layout/SmoothScroller.jsx
+++ src/components/Layout/SmoothScroller.jsx
@@ -17,7 +17,7 @@ export default function SmoothScroll({ children }) {
     const smoother = ScrollSmoother.create({
       wrapper: "#smooth-wrapper",
       content: "#smooth-content",
-      smooth: 2, // how long (in seconds) it takes to "catch up" to the native scroll position
+      smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
       effects: true, // looks for data-speed and data-lag attributes on elements
       smoothTouch: 0.1 // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
     });
